cda8d6e32436aa19417cff3b817514c1318c0873
natively.dev <api@natively.dev>
2026-03-01 17:37:21 +0000

    fix: Resolve iOS build failures and remove deprecated expo-av

--- babel.config.js
+++ babel.config.js
@@ -32,7 +32,7 @@ module.exports = function (api) {
         },
       ],
       "@babel/plugin-proposal-export-namespace-from",
-      "react-native-worklets/plugin"
+      "react-native-reanimated/plugin"
     ],
   };
 };
